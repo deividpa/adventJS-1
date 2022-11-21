9adf5d59891c475e0370ee9f66b270c9224db6f1
David Pérez <deividpa022@gmail.com>
2022-11-20 19:42:50 -0500

    Se agrega el archivo javascript y la información inicial para resolver este ejercico.

---
+++ js/solucion.js
@@ -0,0 +1,18 @@
+const ovejas = [
+  { name: "Noa", color: "azul" },
+  { name: "Euge", color: "rojo" },
+  { name: "Navidad", color: "rojo" },
+  { name: "Ki Na Ma", color: "rojo" },
+  { name: "AAAAAaaaaa", color: "rojo" },
+  { name: "Nnnnnnnn", color: "rojo" },
+];
+
+/* Al ejecutar el método debería devolver lo siguiente:
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
+*/
