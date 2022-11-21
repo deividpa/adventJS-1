b4908b843fe0112afe5c312aa984bc719ed6a5c8
David Pérez <deividpa022@gmail.com>
2022-11-20 20:17:23 -0500

    Se crea la solución del ejercicio haciendo uso de los metodos filter e includes, adicional del metodo de strin toLowerCase para evitar errores al filtrar minúsculas y mayúsculas

--- js/solucion.js
+++ js/solucion.js
@@ -1,3 +1,11 @@
+/*
+Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. 
+Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y 
+que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+
+Por ejemplo, si tenemos las ovejas:
+*/
+
 const ovejas = [
   { name: "Noa", color: "azul" },
   { name: "Euge", color: "rojo" },
@@ -16,3 +24,23 @@ console.log(ovejasFiltradas)
 // [{ name: 'Navidad', color: 'rojo' },
 //  { name: 'Ki Na Ma', color: 'rojo' }]
 */
+
+// Solución
+
+const contarOvejas = (objOve) => {
+  const obejasFiltradas = objOve.filter(({ name, color }) => {
+    let lowerName = name.toLowerCase();
+    console.log(
+      color === "rojo" && lowerName.includes("n") && lowerName.includes("a")
+    );
+    return (
+      color === "rojo" && lowerName.includes("n") && lowerName.includes("a")
+    );
+  });
+
+  return obejasFiltradas;
+};
+
+console.log(contarOvejas(ovejas));
+
+//console.log(ovejasFiltradas);
